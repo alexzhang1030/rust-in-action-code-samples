8b10fa4512a5579e4183caf4cb5bcff6a2e69b8b
alexzhang1030 <1642114555@qq.com>
2022-10-10 23:48:50 +0800

    feat: update ch2 code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod ch2_first_step;
 mod csv_test;
 mod plural_calculation;
+mod while_true_incr_count;
 
 fn main() {
     // crate::csv_test::main();
     // crate::ch2_first_step::main();
-    crate::plural_calculation::main();
+    // crate::plural_calculation::main();
+    crate::while_true_incr_count::main();
 }
